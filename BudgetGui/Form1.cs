8d1e239a3964ef8297f1a3bfcab498c8a2ede75b
iankoratskydcn <44252439+iankoratskydcn@users.noreply.github.com>
2024-02-24 20:34:39 -0500

    added the login screen

--- BudgetGui/Form1.cs
+++ BudgetGui/Form1.cs
@@ -15,10 +15,16 @@ namespace BudgetGui
         private static message_screen message_Screen;
         private static conversation_screen conversation_Screen;
 
+        private static Form1 form1;
+        private static Panel panel_1;
+
+
         public Form1()
         {
 
             InitializeComponent();
+            form1 = this;
+            panel_1 = this.panel1;
             _login_screen = new Login();
             _login_screen.Dock = DockStyle.Fill;
             panel1.Controls.Add(_login_screen);
@@ -59,27 +65,62 @@ namespace BudgetGui
             {
                 case 0:
                     //switch to the login screen and log the user out
+                    panel_1.Controls.Clear();
+
+                    _login_screen = new Login();
+                    _login_screen.Dock = DockStyle.Fill;
+                    form1.panel1.Controls.Add(_login_screen);
 
                     break;
                 case 1:
                     //the user has selected the main screen
+                    panel_1.Controls.Clear();
+
+                    main_Screen = new main_screen();
+                    main_Screen.Dock = DockStyle.Fill;
+                    form1.panel1.Controls.Add(main_Screen);
 
                     break; 
                 case 2:
                     //the user has selected a user view
 
+                    panel_1.Controls.Clear();
+
+                    user_View = new user_view();
+                    user_View.Dock = DockStyle.Fill;
+                    form1.panel1.Controls.Add(user_View);
+
                     break; 
                 case 3:
                     //the user has selected a searched items screen
+
+                    panel_1.Controls.Clear();
+
+                    search_View = new search_view();
+                    search_View.Dock = DockStyle.Fill;
+                    form1.panel1.Controls.Add(search_View);
 
                     break;
 
                 case 4:
                     //the user has selected an item view
 
+
+                    panel_1.Controls.Clear();
+
+                    _item_Full_view = new item_full_view();
+                    _item_Full_view.Dock = DockStyle.Fill;
+                    form1.panel1.Controls.Add(_item_Full_view);
+
                     break;
                 case 5:
                     //the user has selected to view their messages
+
+                    panel_1.Controls.Clear();
+
+                    message_Screen = new message_screen();
+                    message_Screen.Dock = DockStyle.Fill;
+                    form1.panel1.Controls.Add(message_Screen);
 
                     break;
                 case 6:
