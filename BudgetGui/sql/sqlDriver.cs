a35d4382b9f299a856887f1dd06386b47523bdf3
Joshua Bernard <jbernard2024@gmail.com>
2024-03-10 11:26:15 -0400

    SQL Updates

--- BudgetGui/sql/sqlDriver.cs
+++ BudgetGui/sql/sqlDriver.cs
@@ -7,29 +7,37 @@ public class sqlDriver
 
     private string databaseFileName = "budgetList.db";
     private string tablesFileName = "tables.sql";
+    private string fakeDataFileName = "fakeData.sql";
     private string databaseFilePath;
-    private string sqlFilePath;
+    private string tablesFilePath;
+    private string fakeDataFilePath;
 
     public sqlDriver() {
         databaseFilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), databaseFileName);
-        sqlFilePath = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\sql", tablesFileName);
+        tablesFilePath = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\sql", tablesFileName);
+        fakeDataFilePath = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\sql", fakeDataFileName);
     }
 
     public void databaseStartup() {
-        createDatabaseIfNotExists(databaseFilePath, sqlFilePath);
+        createDatabaseIfNotExists(databaseFilePath, tablesFilePath, fakeDataFilePath);
     }
 
-    public void createDatabaseIfNotExists(string databaseFilePath, string sqlFilePath) {
+    public void createDatabaseIfNotExists(string databaseFilePath, string tablesFilePath, string fakeDataFilePath) {
         if (!File.Exists(databaseFilePath)) {
             SQLiteConnection.CreateFile(databaseFilePath);
             Console.WriteLine($"Database '{databaseFilePath}' created successfully.");
-            string sqlCommands = File.ReadAllText(sqlFilePath);
+            string tablesSqlCommands = File.ReadAllText(tablesFilePath);
+            string fakeDataSqlCommands = File.ReadAllText(fakeDataFilePath);
             try {
                 using (SQLiteConnection connection = new SQLiteConnection($"Data Source={databaseFilePath};Version=3;")) {
                     connection.Open();
-                    using (SQLiteCommand command = new SQLiteCommand(sqlCommands, connection)) {
+                    using (SQLiteCommand command = new SQLiteCommand(tablesSqlCommands, connection)) {
                         command.ExecuteNonQuery();
-                        Console.WriteLine("SQL commands executed successfully.");
+                        Console.WriteLine("Table SQL commands executed successfully.");
+                    }
+                    using (SQLiteCommand command = new SQLiteCommand(fakeDataSqlCommands, connection)) {
+                        command.ExecuteNonQuery();
+                        Console.WriteLine("Fake Data SQL commands executed successfully.");
                     }
                 }
             } catch (Exception ex) {
