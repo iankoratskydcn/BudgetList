a280e8d705cf3014d6ce2dbb2dfa0b41c874e413
iankoratskydcn <44252439+iankoratskydcn@users.noreply.github.com>
2024-03-16 14:45:02 -0400

    Revert "Merge pull request #12 from iankoratskydcn/daniel"
    This reverts commit 18ca10d6245dfb356bb06a2d26cd9e4d623affe1, reversing
    changes made to ba1cdfc01aa15acbf0d325d401102530e37c3781.

--- BudgetGui/Screens/Login.Designer.cs
+++ BudgetGui/Screens/Login.Designer.cs
@@ -46,22 +46,20 @@
             // 
             tableLayoutPanel1.BackColor = Color.Transparent;
             tableLayoutPanel1.BackgroundImage = (Image)resources.GetObject("tableLayoutPanel1.BackgroundImage");
-            tableLayoutPanel1.BackgroundImageLayout = ImageLayout.Stretch;
             tableLayoutPanel1.ColumnCount = 3;
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 286F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 200F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 1, 1);
             tableLayoutPanel1.Controls.Add(label3, 1, 0);
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.Location = new Point(0, 0);
-            tableLayoutPanel1.Margin = new Padding(4, 5, 4, 5);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 3;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 333F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 200F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.Size = new Size(1150, 890);
+            tableLayoutPanel1.Size = new Size(805, 534);
             tableLayoutPanel1.TabIndex = 0;
             // 
             // tableLayoutPanel2
@@ -70,7 +68,7 @@
             tableLayoutPanel2.ColumnCount = 3;
             tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 51F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 35F));
             tableLayoutPanel2.Controls.Add(button1, 2, 2);
             tableLayoutPanel2.Controls.Add(textBox2, 1, 2);
             tableLayoutPanel2.Controls.Add(textBox1, 0, 2);
@@ -78,24 +76,22 @@
             tableLayoutPanel2.Controls.Add(label2, 1, 1);
             tableLayoutPanel2.Controls.Add(register, 0, 3);
             tableLayoutPanel2.Dock = DockStyle.Fill;
-            tableLayoutPanel2.Location = new Point(436, 283);
-            tableLayoutPanel2.Margin = new Padding(4, 5, 4, 5);
+            tableLayoutPanel2.Location = new Point(305, 170);
             tableLayoutPanel2.Name = "tableLayoutPanel2";
             tableLayoutPanel2.RowCount = 4;
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 33F));
-            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 47F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 28F));
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel2.Size = new Size(278, 323);
+            tableLayoutPanel2.Size = new Size(194, 194);
             tableLayoutPanel2.TabIndex = 0;
             // 
             // button1
             // 
             button1.Dock = DockStyle.Fill;
-            button1.Location = new Point(230, 159);
-            button1.Margin = new Padding(4, 5, 4, 5);
+            button1.Location = new Point(161, 96);
             button1.Name = "button1";
-            button1.Size = new Size(44, 37);
+            button1.Size = new Size(30, 22);
             button1.TabIndex = 0;
             button1.Text = ">";
             button1.UseVisualStyleBackColor = true;
@@ -104,20 +100,18 @@
             // textBox2
             // 
             textBox2.Dock = DockStyle.Fill;
-            textBox2.Location = new Point(117, 159);
-            textBox2.Margin = new Padding(4, 5, 4, 5);
+            textBox2.Location = new Point(82, 96);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(105, 31);
+            textBox2.Size = new Size(73, 23);
             textBox2.TabIndex = 2;
             textBox2.TextChanged += textBox2_TextChanged;
             // 
             // textBox1
             // 
             textBox1.Dock = DockStyle.Fill;
-            textBox1.Location = new Point(4, 159);
-            textBox1.Margin = new Padding(4, 5, 4, 5);
+            textBox1.Location = new Point(3, 96);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(105, 31);
+            textBox1.Size = new Size(73, 23);
             textBox1.TabIndex = 1;
             textBox1.TextChanged += textBox1_TextChanged;
             // 
@@ -125,10 +119,9 @@
             // 
             label1.AutoSize = true;
             label1.Dock = DockStyle.Bottom;
-            label1.Location = new Point(4, 129);
-            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Location = new Point(3, 78);
             label1.Name = "label1";
-            label1.Size = new Size(105, 25);
+            label1.Size = new Size(73, 15);
             label1.TabIndex = 3;
             label1.Text = "User Name";
             // 
@@ -136,19 +129,17 @@
             // 
             label2.AutoSize = true;
             label2.Dock = DockStyle.Bottom;
-            label2.Location = new Point(117, 129);
-            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Location = new Point(82, 78);
             label2.Name = "label2";
-            label2.Size = new Size(105, 25);
+            label2.Size = new Size(73, 15);
             label2.TabIndex = 4;
             label2.Text = "Password";
             // 
             // register
             // 
-            register.Location = new Point(4, 206);
-            register.Margin = new Padding(4, 5, 4, 5);
+            register.Location = new Point(3, 124);
             register.Name = "register";
-            register.Size = new Size(104, 38);
+            register.Size = new Size(73, 23);
             register.TabIndex = 5;
             register.Text = "Register";
             register.UseVisualStyleBackColor = true;
@@ -159,21 +150,19 @@
             label3.Anchor = AnchorStyles.None;
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            label3.Location = new Point(518, 115);
-            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Location = new Point(363, 67);
             label3.Name = "label3";
-            label3.Size = new Size(114, 48);
+            label3.Size = new Size(78, 32);
             label3.TabIndex = 1;
             label3.Text = "Login";
             // 
             // Login
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             Controls.Add(tableLayoutPanel1);
-            Margin = new Padding(4, 5, 4, 5);
             Name = "Login";
-            Size = new Size(1150, 890);
+            Size = new Size(805, 534);
             tableLayoutPanel1.ResumeLayout(false);
             tableLayoutPanel1.PerformLayout();
             tableLayoutPanel2.ResumeLayout(false);
